3abc64d24693b3fd1b95ac2dcf8031c4ef2c8ff7
unknown <mrampoldi@DP33139.dgeyc>
2013-09-13 15:17:06 -0300

    encapsulo en una funcion

--- inc/botonera.template.php
+++ inc/botonera.template.php
@@ -1,17 +1,20 @@
 <?php 
-	$botones[] = array('nombre'=>'Inicio','link'=>'index.php');
-	$botones[] = array('nombre'=>'Alta de Productos','link'=>'alta-productos.php');
-	$botones[] = array('nombre'=>'Listado de Productos','link'=>'listado-productos.php');
-	$botones[] = array('nombre'=>'Perfil Usuario','link'=>'perfil-usuario.php');
+	function dibujarBotones(){
+		$botones[] = array('nombre'=>'Inicio','link'=>'index.php');
+		$botones[] = array('nombre'=>'Alta de Productos','link'=>'alta-productos.php');
+		$botones[] = array('nombre'=>'Listado de Productos','link'=>'listado-productos.php');
+		$botones[] = array('nombre'=>'Perfil Usuario','link'=>'perfil-usuario.php');
 
-	$botonesHTML = '';
-	// ssi defimo dos variables las toma como key value
-	foreach($botones as $key=>$value){
-		$base='<li><a href="%s">%s</a></li>';
-		$botonesHTML .= sprintf($base, $value['link'], $value['nombre']);
-		$botonesHTML .= "\r\n";
+		$botonesHTML = '';
+		// ssi defimo dos variables las toma como key value
+		foreach($botones as $key=>$value){
+			$base='<li><a href="%s">%s</a></li>';
+			$botonesHTML .= sprintf($base, $value['link'], $value['nombre']);
+			$botonesHTML .= "\r\n";
+		}
+		return $botonesHTML;
 	}
 ?>
 <ul>
-	<?php echo $botonesHTML;?>
+	<?php echo dibujarBotones();?>
 </ul>
