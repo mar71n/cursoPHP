b2a0fbd622ab58c794a28a5d27098455ffa79d38
unknown <mrampoldi@DP33139.dgeyc>
2013-09-13 15:12:16 -0300

    primera clase PHP

---
+++ inc/encabezado.template.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title><?php echo $titulo;?></title>
+		<link href='css/main.css' rel='stylesheet' type='text/css' />
+	</head>
+	<body>
+		<div id='header'>Encabezado con el logo</div>
+		<div id='navbar'>
+			<ul>
+				<li><a href='#'>Boton</a></li>
+				<li><a href='#'>Boton</a></li>
+				<li><a href='#'>Boton</a></li>
+				<li><a href='#'>Boton</a></li>
+				<li><a href='#'>Boton</a></li>
+			</ul>
+		</div>
+		<div id='content'>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+	$titulo='Bienvenido';
+	include('inc/encabezado.template.php');
+	require('inc/index.template.php');
+	include_once('inc/pie.template.php');
+?>
