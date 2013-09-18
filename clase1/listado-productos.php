5aaa5ba571d0894dda7c5c743d33aedb0a4984d0
unknown <mrampoldi@.dgeyc>
2013-09-18 09:14:29 -0300

    resto del menu vacios

---
+++ clase1/listado-productos.php
@@ -0,0 +1,5 @@
+<?php
+	$titulo = 'Listado Productos';
+	include('inc/encabezado.template.php');
+	include_once('inc/pie.template.php');
+?>
---
+++ clase1/perfil.php
@@ -0,0 +1,5 @@
+<?php
+	$titulo = 'Perfil';
+	include('inc/encabezado.template.php');
+	include_once('inc/pie.template.php');
+?>
