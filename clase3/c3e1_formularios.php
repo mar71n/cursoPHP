755450a88d17045865873e60e65439c1c5cd0835
unknown <mrampoldi@bmitre158.dgeyc>
2013-09-26 10:12:00 -0300

    el contenido del checkbox a un array

--- clase3/c3e1_formularios.php
+++ clase3/c3e1_formularios.php
@@ -43,8 +43,8 @@ asi se puede guardar la url a ese estado.
     <input type="radio" name="sex" value="male">Male<br>
     <input type="radio" name="sex" value="female">Female<br>
 
-    <input type="checkbox" name="vehicle" value="Bike">I have a bike<br>
-    <input type="checkbox" name="vehicle" value="Car">I have a car<br>
+    <input type="checkbox" name="vehicle[]" value="Bike">I have a bike<br>
+    <input type="checkbox" name="vehicle[]" value="Car">I have a car<br>
     
     <hr>
     
