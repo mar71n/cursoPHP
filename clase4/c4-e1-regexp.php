d7d96cd354370ebb13be4c0d950ee6238125c859
unknown <mrampoldi@bmitre158.dgeyc>
2013-10-03 10:58:46 -0300

    comienza clase 4

---
+++ clase4/c4-e1-regexp.php
@@ -0,0 +1,18 @@
+<?php
+	$patron = '/^[a-zA-Z0-9_-]{6,16}$/';
+	$valores = array('pepe','pepitos','cañete');
+	echo "<p> Patron : $patron ";
+	echo "<pre>";
+	foreach($valores as $valor){
+		$resultado = preg_match($patron, $valor);
+		var_dump($valor, $resultado);
+	}
+	
+	echo "con filter_var";
+	
+	foreach($valores as $valor){
+		$resultado = filter_var($valor,FILTER_VALIDATE_REGEXP,array('options'=>array('regexp'=>$patron)));
+		var_dump($valor, $resultado);
+	}
+
+?>
