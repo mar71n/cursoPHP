6aa92967720db74dc5319ac6fef677540f81dcad
mar71n <mar71n@gmail.com>
2013-10-29 21:31:37 -0300

    proyecto sergio maquetado

---
+++ proyecto_c4/inc/listado-usuario.template.php
@@ -0,0 +1,10 @@
+<tr class='<?php echo $impar ?>'>
+	<td class='activo'><img src='app_img/usuario_masc.png' /></td>
+	<td class='genero'><?php echo $u['usuario']	?></td>
+	<td>Pepe Biondi</td>
+	<td>sector</td>
+	<td><a href='#'><img src='app_img/email.png' /></a></td>
+	<td>34</td>
+	<td><a href='#'><img src='app_img/editar.png' /></a></td>
+	<td><a href='#'><img src='app_img/borrar.png' /></a></td>
+</tr>
---
+++ proyecto_c4/inc/listado-vacio.template.php
@@ -0,0 +1,6 @@
+<tr>
+	<td colspan='8' class='vacio'>
+		<img src='app_img/exclamacion.png' />
+		No se han encontrado registros
+	</td>
+</tr>
