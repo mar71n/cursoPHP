32d064dfd44313f1703aadd0aa2e8a9b751e9884
unknown <mrampoldi@Bmitre158.dgeyc>
2013-10-31 10:51:14 -0300

    pone el valor desde la base

--- proyecto/inc/listado-usuario.template.php
+++ proyecto/inc/listado-usuario.template.php
@@ -1,10 +1,10 @@
 <tr class='<?php echo $impar ?>'>
 	<td class='activo'><img src='app_img/usuario_masc.png' /></td>
 	<td class='genero'><?php echo $u['usuario']	?></td>
-	<td>Pepe Biondi</td>
-	<td>sector</td>
+	<td><?php echo $u['nombre']?></td>
+	<td><?php echo $u['idsector']?></td>
 	<td><a href='#'><img src='app_img/email.png' /></a></td>
-	<td>34</td>
+	<td><?php echo $u['edad']?></td>
 	<td><a href='#'><img src='app_img/editar.png' /></a></td>
 	<td><a href='#'><img src='app_img/borrar.png' /></a></td>
 </tr>
