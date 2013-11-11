93423bd406f2ad381fdd802746b33a180dd1d390
unknown <mrampoldi@.dgeyc>
2013-11-11 18:03:29 -0300

    prueba: Poner valor de un select en un text

--- clase3/c3-e2_selectDinamico.php
+++ clase3/c3-e2_selectDinamico.php
@@ -14,12 +14,25 @@ echo '</pre>';
 ?>
 <html>
 	<head>
+    <script>
+        function ponervalor(){
+            var destino = document.getElementById("eltexto");
+            var fuente = document.getElementById("elselec");
+            destino.value=fuente.options[fuente.selectedIndex].text;
+        }
+    </script>
 	</head>
 	<body>
 		<form action='' method=''>
 			<?php echo $opciones ;?>
 			<input type='hidden' name='ACTION' value='ALTA_PAIS' />
 			<input type='submit' />
+            <select id="elselec" onchange="ponervalor()">
+                <option value="1">opcion 11</option>
+                <option value="2">opcion 12</option>
+                <option value="3">opcion 13</option>
+            </select>
+            <input type="text" id="eltexto" />
 		</form>
 	</body>
 </html>
