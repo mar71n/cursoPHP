0bc15fdd4846ed50038dbc7298005a422c8055d6
unknown <mrampoldi@.dgeyc>
2013-11-12 15:00:28 -0300

    set de datos de prueba

--- proyecto/db/crear_sectores.sql
+++ proyecto/db/crear_sectores.sql
@@ -1,7 +1,11 @@
-CREATE TABLE sector(
-	idsector INT NOT NULL,
-	nombre VARCHAR(30)
-);
+CREATE TABLE IF NOT EXISTS `sector` (
+  `idsector` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idsector`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
-INSERT INTO sector (idsector, nombre)
-values (1,"administracion"), (2,"procesamiento"), (3,"listado"), (4,"desarrollo") ;
+INSERT INTO `sector` (`idsector`, `nombre`) VALUES
+(1, 'Recursos Humanos'),
+(2, 'Comunicacion'),
+(3, 'Gerencia'),
+(4, 'Campo');
